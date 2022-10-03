df4d87fba03aee3f644a407298bf58ebf3976b2b
catsbe1 <anaribeiro199814@gmail.com>
2022-10-03 21:46:51 +0100

    updated

--- server.js
+++ server.js
@@ -47,6 +47,9 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
     trx
