2194a1de03e205697f8ff723755824ebe2fd6021
catsbe1 <anaribeiro199814@gmail.com>
2022-10-04 02:21:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-triangular-73664",
     user: "postgres",
     password: "DiogoR21",
     database: "smartbrain",
